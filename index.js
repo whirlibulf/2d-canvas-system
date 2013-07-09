0b16515a0166c721081823d1c2bf6a766bfda2a0
whirlibulf <whirlibulf.engine@gmail.com>
2013-07-09 22:23:43 +0100

    Refactor

--- index.js
+++ index.js
@@ -8,42 +8,27 @@ function System(config) {
 }
 
 System.prototype.init = function (engine) {
-    var type,
-        that,
-        options;
-
-    that = this;
+    var options;
 
     console.log("2D Canvas system loaded");
     this.engine = engine;
 
     options = this.engine.config(this.config);
 
-    //canvas element
-    if (options.element) {
-        this.canvas = options.element;
-    } else {
-        this.canvas = this.createCanvas();
-    }
+    //create the canvas element and set up the canvas options
+    this.createCanvas(options);
 
-    if (options.retina === true) {
-        autoscale(this.canvas);
-    }
+    //add the rendering functions
+    this.initRenderables(options.renderables);
 
-    //canvas context
-    this.context = this.canvas.getContext("2d");
+    this.engine.on("componentCreated", this.onComponentCreated.bind(this));
+    this.engine.on("componentRemoved", this.onComponentRemoved.bind(this));
+};
 
-    //canvas size
-    this.width = options.width || this.canvas.width;
-    this.height = options.height || this.canvas.height;
-    if (options.width || options.height) {
-        //Only resize if a custom width/height is set
-        //Otherwise don't change the original canvas element
-        //Since it may already be sized with css or html
-        this.resize();
-    }
+System.prototype.initRenderables = function (renderables) {
+    var type;
 
-    this.renderables = options.renderables || {};
+    this.renderables = renderables || {};
 
     //default renderables
     if (!this.renderables.rectangle) {
@@ -56,37 +41,40 @@ System.prototype.init = function (engine) {
     //instantiate all the render classes
     for (type in this.renderables) {
         if (this.renderables.hasOwnProperty(type)) {
-            if (typeof this.renderables[type] !== "function") {
-                continue;
-            }
-
-            this.renderables[type] = new this.renderables[type](this.engine);
-
-            //render objects need to have a render method
-            if (!this.renderables[type].render) {
-                console.error("Render type '" + type + "' is missing render function");
-                delete this.renderables[type];
-            }
+            this.addRenderable(type);
         }
     }
+};
 
+System.prototype.addRenderable = function (type) {
+    if (typeof this.renderables[type] !== "function") {
+        return;
+    }
 
+    this.renderables[type] = new this.renderables[type](this.engine);
+
+    //render objects need to have a render method
+    if (!this.renderables[type].render) {
+        console.error("Render type '" + type + "' is missing render function");
+        delete this.renderables[type];
+    }
+};
+
+System.prototype.onComponentCreated = function (type, id, entity) {
     //update instance list if a new renderable is created
-    this.engine.on("componentCreated", function (type, id, entity) {
-        if (type === "renderable") {
-            that.renderList.push(that.engine.get(entity, type));
-        }
-    });
+    if (type === "renderable") {
+        this.renderList.push(this.engine.get(entity, type));
+    }
+};
 
+System.prototype.onComponentRemoved = function (type, id) {
     //if a renderable is deleted, reset the instance list completely
     //might be an improvement to loop through the list and simply remove
     //only the deleted component
-    this.engine.on("componentRemoved", function (type, id) {
-        if (type === "renderable") {
-            that.renderList.length = 0;
-            that.renderList = that.engine.getAll('renderable');
-        }
-    });
+    if (type === "renderable") {
+        this.renderList.length = 0;
+        this.renderList = this.engine.getAll("renderable");
+    }
 };
 
 
@@ -99,7 +87,7 @@ function componentSort(a, b) {
 * Call the render function of each component
 */
 System.prototype.render = function () {
-    var i, position, rotation, component;
+    var i, component;
 
     //sort by z index
     this.renderList.sort(componentSort);
@@ -119,33 +107,56 @@ System.prototype.render = function () {
             continue;
         }
 
-
         this.context.save();
-
-        position = this.engine.get(component._entity, 'position');
-        if (position !== undefined) {
-            this.context.translate(position.x, position.y);
-        } else {
-            //Don't draw something if it doesn't have a position
-            this.context.restore();
-            continue;
-        }
-
-        rotation = this.engine.get(component._entity, 'rotation');
-        if (rotation !== undefined) {
-            this.context.rotate(rotation.angle);
-        }
-
-        //TODO: add scale
-
-        this.renderables[component.type].render(this.context, component._entity);
-
+        this.renderComponent(component);
         this.context.restore();
     }
 };
 
-System.prototype.createCanvas = function () {
-    return document.createElement("canvas");
+System.prototype.renderComponent = function (component) {
+    var position, rotation;
+
+    position = this.engine.get(component._entity, "position");
+    if (position === undefined) {
+        //Don't draw something if it doesn't have a position
+        return;
+    }
+    this.context.translate(position.x, position.y);
+
+    rotation = this.engine.get(component._entity, "rotation");
+    if (rotation !== undefined) {
+        this.context.rotate(rotation.angle);
+    }
+
+    //TODO: add scale
+
+    this.renderables[component.type].render(this.context, component._entity);
+};
+
+System.prototype.createCanvas = function (options) {
+    if (!options.element) {
+        this.canvas = document.createElement("canvas");
+    } else {
+        this.canvas = options.element;
+    }
+
+    //retina scaling
+    if (options.retina === true) {
+        autoscale(this.canvas);
+    }
+
+    //canvas size
+    this.width = options.width || this.canvas.width;
+    this.height = options.height || this.canvas.height;
+    if (options.width || options.height) {
+        //Only resize if a custom width/height is set
+        //Otherwise don't change the original canvas element
+        //Since it may already be sized with css or html
+        this.resize();
+    }
+
+    //canvas context
+    this.context = this.canvas.getContext("2d");
 };
 
 System.prototype.resize = function () {
